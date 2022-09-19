d431b24a792b41786acb97b70a8faaf9c98bc5a9
“AdeleAps” <adelea@inbox.lv>
2022-09-20 00:45:44 +0300

    Add basic routing for main pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
-import styles from './App.module.scss'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import styles from "./App.module.scss";
+import Home from "../src/pages/home";
 
 function App() {
-
   return (
     <div className={styles.App}>
-      NavBar App
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} caseSensitive={false} />
+          <Route path="/home" element={<Home />} caseSensitive={false} />
+          <Route path="/locations" caseSensitive={false} />
+          
+          // 404 not found goes here
+          <Route path="*" caseSensitive={false} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
