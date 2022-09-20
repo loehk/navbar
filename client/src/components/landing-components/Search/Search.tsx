ba3befd6f8c64c77c34c972b48e8545999bc3c4a
“AdeleAps” <adelea@inbox.lv>
2022-09-21 00:29:07 +0300

    Remove redundant code in search component

--- client/src/components/landing-components/Search/Search.tsx
+++ client/src/components/landing-components/Search/Search.tsx
@@ -1,7 +1,6 @@
 import styles from "./Search.module.scss";
 import SearchIcon from '@mui/icons-material/Search';
-import { TextField } from '@mui/material';
-import { Button } from '@mui/material';
+import { TextField, Button } from '@mui/material';
 
 type Props = {}
 
@@ -9,7 +8,7 @@ function Search({}: Props) {
   return (
     <div className={styles.searchContainer}>
         <SearchIcon  sx={{ fontSize: 30, color: '#7d6b91'}} />
-        <TextField variant="standard" placeholder="Search" />
+        <TextField sx={{ width: "80%"}} variant="standard" placeholder="Search" />
         <Button sx={{ marginLeft: 10, textTransform: "none", fontSize: 15}} size="large" variant="contained">Search</Button>
     </div>
   )
