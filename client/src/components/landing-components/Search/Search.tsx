b9a1d7b88b89e6c960057a1a2cb9c15dd93b26fc
“AdeleAps” <adelea@inbox.lv>
2022-09-21 20:02:12 +0300

    Modify styling for search component, add a zoom animation for the first carousel slide

--- client/src/components/landing-components/Search/Search.tsx
+++ client/src/components/landing-components/Search/Search.tsx
@@ -8,10 +8,10 @@ type Props = {};
 function Search({}: Props) {
   return (
     <motion.div initial={{y: 100, opacity: 0.2}} animate={{y: 0, opacity: 1}} transition={{ duration: 0.5, delay: 0.2}} className={styles.searchContainer}>
-      <SearchIcon sx={{ fontSize: 30, color: '#7d6b91', marginRight: 2 }} />
+      <SearchIcon sx={{ fontSize: 30, color: '#7d6b91', margin: 1.5 }} />
       <TextField
         sx={{
-          width: '80%',
+          width: '100%',
           '& .MuiInput-underline:before': { borderBottomColor: 'rgb(204, 202, 202)' },
           '& .MuiInput-underline:after': { borderBottomColor: '#7d6b91' },
         }}
@@ -19,7 +19,7 @@ function Search({}: Props) {
         placeholder="Search"
       />
       <Button
-        sx={{ marginLeft: 10, textTransform: 'none', fontSize: 15, width: '30%', padding: 1.6 }}
+        sx={{ marginLeft: 5, textTransform: 'none', fontSize: 15, width: '45%', padding: 1.6 }}
         size="large"
         variant="contained"
       >
