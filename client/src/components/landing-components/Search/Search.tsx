6ec5d9a6350e33b6e6e8d34b56b94f5b165e2ba9
“AdeleAps” <adelea@inbox.lv>
2022-09-22 20:33:46 +0300

    Restrict autocomplete search results to nearby bars

--- client/src/components/landing-components/Search/Search.tsx
+++ client/src/components/landing-components/Search/Search.tsx
@@ -2,17 +2,17 @@ import styles from './Search.module.scss';
 import SearchIcon from '@mui/icons-material/Search';
 import { TextField, Button } from '@mui/material';
 import { motion } from 'framer-motion';
-import { StandaloneSearchBox } from '@react-google-maps/api';
+import { StandaloneSearchBox, GoogleMap, Autocomplete } from '@react-google-maps/api';
 import { useState, useRef, useEffect } from 'react';
 
 type Props = {};
 
 function Search({}: Props) {
-  const inputRef = useRef(null);
+  const inputRef = useRef<any>(null);
 
-  const [searchBox, setSearchBox] = useState(null);
+  const [searchBox, setSearchBox] = useState<any>(null);
 
-  // in case of geotracking issues, the default value is the center of Riga
+  // in case of location service issues, the default value is the center of Riga
   const [currentLocation, setCurrentLocation] = useState({
     latitude: 56.95,
     longitude: 24.1,
@@ -24,21 +24,20 @@ function Search({}: Props) {
       enableHighAccuracy: true,
     };
 
-    const error = (error: any) => {
+    const error = (error: GeolocationPositionError) => {
       console.error('Failed to fetch location! Please enable location services in your browser.');
     };
-    const success = (location: any) => {
+    const success = (location: GeolocationPosition) => {
       setCurrentLocation({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
       });
     };
-
     navigator.geolocation.getCurrentPosition(success, error, options);
   }, []);
 
   const onPlacesChanged = () => {
-    console.log(searchBox.getPlaces());
+    console.log(searchBox!.getPlaces());
   };
 
   const onLoadHandler = (ref: any) => {
@@ -46,7 +45,7 @@ function Search({}: Props) {
   };
 
   const changeHandler = () => {
-    console.log(inputRef.current.value);
+    console.log(inputRef?.current?.value);
   };
 
   const coordinates = new google.maps.LatLng(currentLocation.latitude, currentLocation.longitude);
@@ -57,8 +56,11 @@ function Search({}: Props) {
     240,
   );
   const ne = google.maps.geometry.spherical.computeOffset(coordinates.toJSON(), 500, 60);
-
   const mapBounds = new google.maps.LatLngBounds(swOrLatLngBounds.toJSON(), ne.toJSON());
+  const options = {
+    types: ["bar"],
+  }
+
 
   return (
     <motion.div
@@ -68,23 +70,21 @@ function Search({}: Props) {
       className={styles.searchContainer}
     >
       <SearchIcon sx={{ fontSize: 30, color: '#7d6b91', margin: 1.5 }} />
-      <StandaloneSearchBox
-        bounds={mapBounds}
-        onPlacesChanged={onPlacesChanged}
-        onLoad={onLoadHandler}
-      >
-        <TextField
-          onChange={changeHandler}
-          inputRef={inputRef}
-          sx={{
-            width: '100%',
-            '& .MuiInput-underline:before': { borderBottomColor: 'rgb(204, 202, 202)' },
-            '& .MuiInput-underline:after': { borderBottomColor: '#7d6b91' },
-          }}
-          variant="standard"
-          placeholder="Search"
-        />
-      </StandaloneSearchBox>
+      <GoogleMap>
+        <Autocomplete options={options} bounds={mapBounds} onPlaceChanged={onPlacesChanged} onLoad={onLoadHandler}>
+          <TextField
+            onChange={changeHandler}
+            inputRef={inputRef}
+            sx={{
+              width: '100%',
+              '& .MuiInput-underline:before': { borderBottomColor: 'rgb(204, 202, 202)' },
+              '& .MuiInput-underline:after': { borderBottomColor: '#7d6b91' },
+            }}
+            variant="standard"
+            placeholder="Search"
+          />
+        </Autocomplete>
+      </GoogleMap>
       <Button
         sx={{ marginLeft: 3, textTransform: 'none', fontSize: 15, width: '30%', padding: 1.6 }}
         size="large"
