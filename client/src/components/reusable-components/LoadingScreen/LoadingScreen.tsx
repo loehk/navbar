49584e5858d47fd92e464ede9c09a79e11682f8c
“AdeleAps” <adelea@inbox.lv>
2022-09-25 21:46:12 +0300

    Add loading animation

--- client/src/components/reusable-components/LoadingScreen/LoadingScreen.tsx
+++ client/src/components/reusable-components/LoadingScreen/LoadingScreen.tsx
@@ -1,11 +1,14 @@
-import styles from "./LoadingScreen.module.scss";
+import styles from './LoadingScreen.module.scss';
+import { motion } from 'framer-motion';
 
-type Props = {}
+type Props = {};
 
 const LoadingScreen = (props: Props) => {
   return (
-    <div className={styles.LoadingScreenWrapper}>LOADING...</div>
-  )
-}
+    <div className={styles.LoadingScreenWrapper}>
+      <motion.img animate={{ rotate: 360 }} transition={{ repeat: Infinity }} src="./icons/icon.svg" alt="NavBar logo" />
+    </div>
+  );
+};
 
-export default LoadingScreen
+export default LoadingScreen;
